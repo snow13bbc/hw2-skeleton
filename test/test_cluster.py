3519eaf22f2c0edd72ad949ea2a2b8aac032bbed
snow13bbc <snow.naing@ucsf.edu>
2018-02-09 23:47:35 -0800

    Adding test cases to work with my functions

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -4,7 +4,7 @@ import os
 
 def test_similarity():
     filename_a = os.path.join("data", "276.pdb")
-    filename_b = os.path.join("data", "4629.pdb")
+    filename_b = os.path.join("data", "1806.pdb")
 
     activesite_a = io.read_active_site(filename_a)
     activesite_b = io.read_active_site(filename_b)
@@ -14,24 +14,34 @@ def test_similarity():
 
 def test_partition_clustering():
     # tractable subset
-    pdb_ids = [276, 4629, 10701]
+    pdb_ids = [276, 39299, 38031]
 
     active_sites = []
     for id in pdb_ids:
         filepath = os.path.join("data", "%i.pdb"%id)
         active_sites.append(io.read_active_site(filepath))
+    sim_matrix = cluster.similarity_matrix(active_sites)
 
-    # update this assertion
-    assert cluster.cluster_by_partitioning(active_sites) == []
+    assert cluster.cluster_by_partitioning([], {}) == []
+    assert cluster.cluster_by_partitioning([active_sites[0]], {}) == [[active_sites[0]]]
+    clustering = cluster.cluster_by_partitioning(active_sites, sim_matrix)
+    assert len(clustering) == 2
+
 
 def test_hierarchical_clustering():
     # tractable subset
-    pdb_ids = [276, 4629, 10701]
+    pdb_ids = [276, 52954, 34088]
 
     active_sites = []
     for id in pdb_ids:
         filepath = os.path.join("data", "%i.pdb"%id)
         active_sites.append(io.read_active_site(filepath))
 
+    sim_matrix = cluster.similarity_matrix(active_sites)
+
     # update this assertion
-    assert cluster.cluster_hierarchically(active_sites) == []
+    assert cluster.cluster_hierarchically([], {}) == []
+    assert cluster.cluster_hierarchically([active_sites[0]], {}) == [[active_sites[0]]]
+    clustering = cluster.cluster_hierarchically(active_sites, sim_matrix)
+    assert clustering[0] == [active_sites[2]]
+    assert set(clustering[1]) == set([active_sites[0], active_sites[1]])

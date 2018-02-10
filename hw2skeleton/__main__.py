234ebc18f3e8ed7d07833ab71bb9953d49837f3c
snow13bbc <snow.naing@ucsf.edu>
2018-02-09 23:45:45 -0800

    Importing functions from cluster.py

--- hw2skeleton/__main__.py
+++ hw2skeleton/__main__.py
@@ -1,10 +1,10 @@
 import sys
 from .io import read_active_sites, write_clustering, write_mult_clusterings
-from .cluster import cluster_by_partitioning, cluster_hierarchically
+from .cluster import cluster_by_partitioning, cluster_hierarchically, similarity_matrix, quality_score, comparison_score, compare
 
 # Some quick stuff to make sure the program is called correctly
 if len(sys.argv) < 4:
-    print("Usage: python -m hw2skeleton [-P| -H] <pdb directory> <output file>")
+    print("Usage: python -m hw2skeleton [-P| -H| -C] <pdb directory> <output file>")
     sys.exit(0)
 
 active_sites = read_active_sites(sys.argv[2])
@@ -12,10 +12,19 @@ active_sites = read_active_sites(sys.argv[2])
 # Choose clustering algorithm
 if sys.argv[1][0:2] == '-P':
     print("Clustering using Partitioning method")
-    clustering = cluster_by_partitioning(active_sites)
+    sim_matrix = similarity_matrix(active_sites)
+    clustering = cluster_by_partitioning(active_sites, sim_matrix)
     write_clustering(sys.argv[3], clustering)
 
 if sys.argv[1][0:2] == '-H':
     print("Clustering using hierarchical method")
-    clusterings = cluster_hierarchically(active_sites)
-    write_mult_clusterings(sys.argv[3], clusterings)
+    sim_matrix = similarity_matrix(active_sites)
+    clustering = cluster_hierarchically(active_sites, sim_matrix)
+    write_clustering(sys.argv[3], clustering)
+
+if sys.argv[1][0:2] == '-C':
+    print("Comparing hierarchical and partioning methods")
+    sim_matrix = similarity_matrix(active_sites)
+    hierarchical_clustering = cluster_hierarchically(active_sites, sim_matrix)
+    partioning_clustering = cluster_by_partitioning(active_sites, sim_matrix)
+    compare(hierarchical_clustering, partioning_clustering, active_sites, sim_matrix)
